1b92e34bb58c9dab0596b6b78c8f26478defb033
satya M <satya2maity@gmail.com>
2023-04-27 01:56:42 +0530

    added BasicJavascript

--- Class-1/index2.js
+++ Class-1/index2.js
@@ -3,4 +3,10 @@
 let firstname="Satyaranjan";
 
 let lastname=firstname.slice(0,5)
-console.log(lastname);
+// console.log(lastname);
+
+
+// console.log(typeof null);
+
+console.log(Number.MAX_SAFE_INTEGER);
+
