cdbe35c865a4022a207c1f31d57f59451ad6c3f7
satya M <satya2maity@gmail.com>
2023-04-27 01:02:58 +0530

    added BasicJavascript

--- Class-1/index.js
+++ Class-1/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+// console.log("Hello World");
+'use strict';
+
+//variable Dicleration
+var name="Satyaranjan";
+//Print the value
+console.log(name)
+
+name="Biplab"
+
+console.log(name);
+
+//Change value
+
