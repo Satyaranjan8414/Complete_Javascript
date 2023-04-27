b55200dbe9f9308415be2fbbe026a2e8960a1ad1
satya M <satya2maity@gmail.com>
2023-04-28 02:28:41 +0530

    added if/Else-if/Else

---
+++ Class-1/index6.js
@@ -0,0 +1,35 @@
+// let day=0
+// By using if/else-if/Else codition
+
+//Daily calendar 
+
+
+let day=+prompt("Plaese Gusess the day")
+
+if(day==0){
+    console.log("sunday");
+    
+}else if(day==1){
+    console.log("Monday");
+    
+}else if(day==2){
+    console.log("Tuesday");
+    
+}else if(day==3){
+    console.log("Wednesday");
+    
+}else if(day==4){
+    console.log("Thursday");
+    
+}else if(day==5){
+    console.log("Friday");
+    
+}else if(day==6){
+    console.log("Saturday");
+    
+}else{
+    console.log("Plaese Enter a valid day");
+    
+}
+
+//By Using Switch Statement
