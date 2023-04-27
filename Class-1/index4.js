7815181a9b1c9c51e37a453bda30c4cc15dd6eab
satya M <satya2maity@gmail.com>
2023-04-28 02:00:44 +0530

    added ifElse

---
+++ Class-1/index4.js
@@ -0,0 +1,21 @@
+let name="sai"
+let age=23;
+// conditional operator 
+
+if(name=="sa"){
+    console.log("Welcome sai");
+    
+}else{
+    console.log("Welcome Guest");
+    
+}
+
+// If i want to check Both The codition 
+
+if(name=="sai" && age==23){
+    console.log("You can login");
+    
+}else{
+    console.log("You cannot login");
+    
+}
