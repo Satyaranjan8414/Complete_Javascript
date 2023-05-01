3b80ae78825b65ba8f04dadace99a0d301ce2fa7
satya M <satya2maity@gmail.com>
2023-05-02 02:43:33 +0530

    added new

--- Class-1/index9.js
+++ Class-1/index9.js
@@ -4,8 +4,11 @@ let num=10
 
 for(let i=0;i<=num;i++){
     if(i==7){
-        break;
+        // break;
+        continue;
     }
     sum+=i
 }
 console.log(sum)
+
+// console.log(1+2+3+4+5+6+8+9+10)
