6c798bd124b2e5ab4b739a1da90fd2083029e7db
satya M <satya2maity@gmail.com>
2023-05-02 03:06:10 +0530

    added new

---
+++ Class-1/index11.js
@@ -0,0 +1,20 @@
+//Collection Of Items in order manner
+
+//Array is mutuble we can change the value at anytime but Before excute the code
+
+
+// let name=["Satya","Santosh","Govind"]
+//  name[1]="Sonu"
+// console.log(name)
+
+// console.log(typeof(name))
+// console.log(Array.isArray(name))
+
+
+//Array Push POP
+
+let name=["Satya","Santosh","Govind"]
+
+name.push("sai")
+name.pop()
+console.log(name)
