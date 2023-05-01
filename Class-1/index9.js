5d055de70e1c7dc4aca4a01646f083f4c12b5baa
satya M <satya2maity@gmail.com>
2023-05-02 02:48:18 +0530

    added new

--- Class-1/index9.js
+++ Class-1/index9.js
@@ -1,14 +1,25 @@
 // ForLoos
-let sum=0;
-let num=10
+// let sum=0;
+// let num=10
 
-for(let i=0;i<=num;i++){
-    if(i==7){
-        // break;
-        continue;
-    }
-    sum+=i
-}
-console.log(sum)
+// for(let i=0;i<=num;i++){
+//     if(i==7){
+//         // break;
+//         continue;
+//     }
+//     sum+=i
+// }
+// console.log(sum)
 
 // console.log(1+2+3+4+5+6+8+9+10)
+
+// DoWhile LOOPS
+
+let i=0;
+do{
+    console.log(i)
+    i++;
+}while(i<=9)
+
+
+
