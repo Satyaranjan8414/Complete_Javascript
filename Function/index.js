161de7c003bb6bce705855d94c7fbacd1ce6295f
satya M <satya2maity@gmail.com>
2023-05-05 02:01:37 +0530

    added function

---
+++ Function/index.js
@@ -0,0 +1,27 @@
+//Function is nothing but repeatation same code again and again without Rewrite the code
+
+function happyBirthday(){
+    console.log("Happy Birthday To You Happy BirthDay To Gn....du......")
+}
+
+// happyBirthday()
+
+// happyBirthday()
+
+// happyBirthday()
+
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
+
+
+function Addition(a,b){
+    console.log(a+b)
+}
+
+Addition(2,3)
+Addition(2,9)
