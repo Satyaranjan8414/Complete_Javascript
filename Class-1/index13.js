ac3889c4b3acb41793ad54ffafea455845d2f6d6
satya M <satya2maity@gmail.com>
2023-05-04 09:07:03 +0530

    added fullArr

--- Class-1/index13.js
+++ Class-1/index13.js
@@ -2,22 +2,33 @@
 
 //Object Always Referance Type
 
-const car={
-    name:"tesla",
-    wheel:4,
-    color:["Black","Red","Green","Yellow"]
-};
+// const car={
+//     name:"tesla",
+//     wheel:4,
+//     color:["Black","Red","Green","Yellow"]
+// };
 // console.log(car.name);
 // console.log(car.wheel);
-
-car.seat="Front";
+// 
+// car.seat="Front";
 // console.log(car);
 
 // console.log(car["name"]);
 // console.log(car["wheel"]);
 
-for(let key in car){
-    console.log(car[key]);
+// for(let key in car){
+//     console.log(key,": ",car[key]);
+// }
+
+let key1="objectkey1";
+let key2="objectkey2";
+
+let Value1="objectvalue1";
+let Value2="objectvalue2";
+
+let obj={
+    [key1]:Value1,
+   [ key2]:Value2,
 }
 
-
+console.log(obj);
