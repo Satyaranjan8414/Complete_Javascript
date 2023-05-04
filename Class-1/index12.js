2515cc9c78c6c72591fe34aff18fc71d15d9de40
satya M <satya2maity@gmail.com>
2023-05-04 08:38:55 +0530

    added fullArr

---
+++ Class-1/index12.js
@@ -0,0 +1,10 @@
+// Array Destruring
+
+const myarr=["value1","value2","valuu3","value4"]
+
+
+const [myvar1,myvar2,...mynewarr]=myarr;
+
+console.log(myvar1)
+console.log(myvar2)
+console.log(mynewarr)
