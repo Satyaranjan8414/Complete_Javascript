e1149ddb23b317dfaa91c55cf502cd3da4ac5add
satya M <satya2maity@gmail.com>
2023-05-04 12:59:23 +0530

    added object

--- Class-1/index13.js
+++ Class-1/index13.js
@@ -9,7 +9,7 @@
 // };
 // console.log(car.name);
 // console.log(car.wheel);
-// 
+//
 // car.seat="Front";
 // console.log(car);
 
@@ -20,48 +20,43 @@
 //     console.log(key,": ",car[key]);
 // }
 
-let key1="objectkey1";
-let key2="objectkey2";
+let key1 = "objectkey1";
+let key2 = "objectkey2";
 
-let Value1="objectvalue1";
-let Value2="objectvalue2";
+let Value1 = "objectvalue1";
+let Value2 = "objectvalue2";
 
-let obj={
-    [key1]:Value1,
-   [ key2]:Value2,
-}
+let obj = {
+  [key1]: Value1,
+  [key2]: Value2,
+};
 
 // console.log(obj);
 
-
 //Apred Operators
 
-const obj1={
-    name1:"apred",
-    age1:22
+const obj1 = {
+  name1: "apred",
+  age1: 22,
 };
-const obj2={
-    name:"paped",
-    age:25
+const obj2 = {
+  name: "paped",
+  age: 25,
 };
 
-const newobj={...obj1,...obj2}
+const newobj = { ...obj1, ...obj2 };
 console.log(newobj);
 
+//Object Destructuring
 
-//Object Destructuring 
+const school = {
+  name1: "Raju",
+  name2: "Rahul",
+};
 
-const school={
-    name1:"Raju",
-    name2:"Rahul"
-}
-
-const {name1:var1,name2:var2}=school;
+const { name1: var1, name2: var2 } = school;
 console.log(var1);
 console.log(var2);
-
-
-
 
 // const school2={
 //     name1:"Raju",
@@ -72,3 +67,24 @@ console.log(var2);
 // console.log(var1);
 // console.log(var2);
 
+//Object Inside Array
+
+const user = [
+  {
+    name: "Satya",
+    gender: "male",
+    age: 23,
+  },
+  {
+    name: "sai",
+    gender: "male",
+    age: 23,
+  },
+  {
+    name: "Satya",
+    gender: "male",
+    age: 23,
+  },
+];
+
+console.log(user);
