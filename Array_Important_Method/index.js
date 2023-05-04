77f93176fe932f366db19842486e6283cbc3d717
satya M <satya2maity@gmail.com>
2023-05-05 05:23:41 +0530

    added mapMethod

--- Array_Important_Method/index.js
+++ Array_Important_Method/index.js
@@ -1,7 +1,7 @@
 //Foreach
 //Map
 //Filters
-const arr=[4,5,8,9]
+// const arr=[4,5,8,9]
 
 // function myfun(number,i){
 //     console.log(`index is ${i} number is ${number}`)
@@ -13,6 +13,55 @@ const arr=[4,5,8,9]
 // }
 // arr.forEach(myfun)
 
-arr.forEach(function(number,i){
-    console.log(`index is ${i} number is ${number*2}`)
+// arr.forEach(function(number,i){
+//     console.log(`index is ${i} number is ${number*2}`)
+// })
+
+// const user=[
+//     {name:"Satya",age:22},
+//     {name:"santosh",age:25},
+//     {name:"Govind",age:30},
+//     {name:"Shravani",age:26},
+// ]
+
+// //Application Area of ForEach
+// user.forEach(function(user){
+//     console.log(user.name)
+// })
+
+// //fOR Of Loop
+// for(let users of user){
+//     console.log(users.name)
+// }
+
+
+// //By Using Arrow Function
+// user.forEach((user)=>{
+//     console.log(user.name)
+// })
+
+// //MAP METHOD
+// const arr=[3,4,7,8];
+// const root=(arr)=>{
+//     return arr/2
+// }
+
+// const res=arr.map(root)
+// console.log(res)
+
+
+const user=[
+    {name:"Satya",age:22},
+    {name:"santosh",age:25},
+    {name:"Govind",age:30},
+    {name:"Shravani",age:26},
+];
+
+let alluser=user.map((user)=>{
+    return `Name:${user.name} age: ${user.age}` 
 })
+
+console.log(alluser)
+
+
+
