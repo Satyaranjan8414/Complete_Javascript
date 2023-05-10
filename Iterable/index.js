3c3a076ce2b0b353fc51171b57517921f404d008
satya M <satya2maity@gmail.com>
2023-05-10 09:59:31 +0530

    added Iterable

--- Iterable/index.js
+++ Iterable/index.js
@@ -1,6 +1,11 @@
 // Iterable 7:52
 
-let name="Satyaranjan Maity"
-for(let i =0;i<name.length;i++){
-    console.log(name[i])
-}
+// let name="Satyaranjan Maity"
+// for(let i =0;i<name.length;i++){
+//     console.log(name[i])
+// }
+
+//Set Iterable
+
+const numbers=new Set([1,2,3]);
+console.log(numbers)
