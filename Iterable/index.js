c7cb0f0da76caea5d089b386544a921a5599e9ba
satya M <satya2maity@gmail.com>
2023-05-10 10:08:47 +0530

    added Iterable

--- Iterable/index.js
+++ Iterable/index.js
@@ -7,12 +7,21 @@
 
 //Set Iterable
 
-const numbers=new Set([2,3]);
-numbers.add(6)
-if(numbers.has(1)){
-    console.log("1 is Present")
-}else{
-    console.log("No")
-}
-console.log(numbers)
+// const numbers=new Set([2,3]);
+// numbers.add(6)
+// if(numbers.has(1)){
+//     console.log("1 is Present")
+// }else{
+//     console.log("No")
+// }
+// console.log(numbers)
+
+
+//fIND THE Unique Elemrnt in the gIVEN aARRAY
+
+const myarr=[1,2,3,4,4,5,6,6]
+
+const unique=new Set(myarr)
+
+console.log(unique)
 
