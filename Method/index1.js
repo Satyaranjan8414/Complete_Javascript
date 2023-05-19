927a227440d81d026ec2a6d4682a70a9ee6d4ad5
satya M <satya2maity@gmail.com>
2023-05-19 23:43:12 +0530

    added user

---
+++ Method/index1.js
@@ -0,0 +1,37 @@
+const userMethods= {
+    about:function(){
+    return `${this.firstname} is ${this.age} Year Old`
+},
+is18:function(){
+    return this.age>=18
+}
+// sing:function(){
+//     return 
+// }
+}
+
+
+
+
+
+function CreateUser(firstname,lastname,email,age,address){
+    const user={};
+    user.firstname=firstname;
+    user.lastname=lastname;
+    user.email=email;
+    user.age=age;
+    user.address=address;
+    user.about=userMethods.about;
+    user.is18=userMethods.is18;
+    return user;
+   
+}
+
+const user1=CreateUser("Satya","Maity","Satya@maity@gmail.com",19,"Bengalore")
+// const user2=crea
+// const about=user1.about()
+// const is18=user1.is18()
+
+console.log(user1)
+console.log(user1.about());
+console.log(user1.is18());
