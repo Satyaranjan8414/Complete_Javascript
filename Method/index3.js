a0de5fe7b9cd78a29a59fd3639c3516021a2548d
satya M <satya2maity@gmail.com>
2023-05-20 18:02:23 +0530

    added NewKeywords

--- Method/index3.js
+++ Method/index3.js
@@ -1,13 +1,14 @@
 //New Keywords :---
 
 
-function CreateUser(firstname,lastname,email,age,address){
+function CreateUser(firstname,lastname,email,age,address,sing){
     // const user=Object.create(CreateUser.prototype);
     this.firstname=firstname;
     this.lastname=lastname;
     this.email=email;
     this.age=age;
     this.address=address;
+    this.sing=sing;
    
 }
 CreateUser.prototype.about=function(){
@@ -23,7 +24,14 @@ CreateUser.prototype.sing=function(){
 
 const user1= new CreateUser("Satya","Maity","Satya@maity@gmail.com",19,"Bengalore")
 
-console.log(user1)
+const user2= new CreateUser("Sonalika","Das","Sonalika6666@gmail.com",22,"Balasore","Love aj kal")
+
+// console.log(user1)
+console.log(user2)
 // console.log(user1.about());
 // console.log(user1.is18());
 // console.log(user1.sing());
+
+console.log(user2.about());
+console.log(user2.is18());
+console.log(user2.sing);
