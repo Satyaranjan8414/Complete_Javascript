747bdf46ff6d95323e7f34e329c801ae879ff630
satya M <satya2maity@gmail.com>
2023-05-23 02:22:20 +0530

    added CodeCreationAndExecutionPhase

--- class-2/CodeExecution/index.js
+++ class-2/CodeExecution/index.js
@@ -1,8 +1,15 @@
 console.log(this)
 console.log(name);
 console.log(window);
+console.log(myfun)
+function myfun(){
+    console.log("My name is Myfun")
+}
 
+var fname="Satya";
+var lname="Maity";
+var fullname=fname+" "+lname
+console.log(fullname)
 
-var name="Satya";
 
 console.log(name)
