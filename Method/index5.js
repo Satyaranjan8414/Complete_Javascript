0ddea38f3e985a4cb08cefe54e0d2d405dec7877
satya M <satya2maity@gmail.com>
2023-05-22 21:36:38 +0530

    GetterandSetter

--- Method/index5.js
+++ Method/index5.js
@@ -9,15 +9,24 @@ class person{
    get fullname(){
         return `${this.firstname} ${this.lastname} ${this.age}`;
     }
-    setname(firstname,lastname){
+    // setname(firstname,lastname){
+    //     this.firstname=firstname;
+    //     this.lastname=lastname;
+    // }
+
+    set setname(fullname){
+        const [firstname,lastname] = fullname.split(" ");
         this.firstname=firstname;
         this.lastname=lastname;
+
     }
 }
 const person1=new person("SaraAli","Khan",23);
 console.log(person1.fullname);
 
- person1.setname("Rahul","Reddy")
-console.log(person1.firstname);
-console.log(person1.lastname);
+//  person1.setname("Rahul","Reddy")
+// console.log(person1.firstname);
+// console.log(person1.lastname);
+
+
 
