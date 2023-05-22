5c8441ce661469efa85747c02a648a2575301cfb
satya M <satya2maity@gmail.com>
2023-05-22 16:12:35 +0530

    added Constructor_Function

--- Method/index5.js
+++ Method/index5.js
@@ -1,2 +1,12 @@
 //Getter and setter functions
 
+class person{
+    constructor(firstname,lastname,age){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.age=age;
+    }
+}
+const person1=new person("SaraAli","Khan",23);
+
+console.log(person1)
