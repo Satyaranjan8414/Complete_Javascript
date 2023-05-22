61852dd5bfddd7bc5e8c364f8a7e2b0e00b75416
satya M <satya2maity@gmail.com>
2023-05-23 03:42:01 +0530

    added closures

--- class-2/CodeExecution/index4.js
+++ class-2/CodeExecution/index4.js
@@ -1,11 +1,22 @@
 //Closures are used 
 
-function myfun(){
-    function child(){
-        console.log("This is Child")
-    }
-    return child;
+// function myfun(fname,lname){
+//     function child(fname,lname){
+//         console.log(fname,lname);
+//     }
+//     return child;
 
+// }
+// const result = myfun("Satya","Maity");
+
+// console.log(result)
+
+function hello(x){
+    const a="Hello"
+    const b="World"
+    return function(){
+        console.log(a,b,x)
+    }
 }
-const result = myfun();
-result();
+const result = hello("arg")
+result()
