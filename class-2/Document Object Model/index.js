1944770abbd28c11d21f1facf19d3464db548d62
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-03 02:13:00 +0530

    added data

--- class-2/Document Object Model/index.js
+++ class-2/Document Object Model/index.js
@@ -2,16 +2,6 @@
 
 // select a document from the document collection ids
 
-// const worktime= document.getElementById("works-time")
-// console.log(worktime)
-
-
-//SELECT A DOCUMENT BY USING QUERY SELECTORS
-
-// const worktime2= document.querySelector("#works-time")
-// console.log(worktime2)
-
-//SELECT A DOCUMENT BY USING QUERY SELECTORS
-
-const workTime2=document.querySelector(".navbar")
-console.log(workTime2)
+// const worktime=document.getElementById("works-time")
+const link=document.querySelector("a")
+console.log(link.getAttribute("href").slice(1))
