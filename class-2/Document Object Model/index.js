4c47fa12868c85068a2963227bbf0c2362eca6d8
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-03 22:45:09 +0530

    added new

--- class-2/Document Object Model/index.js
+++ class-2/Document Object Model/index.js
@@ -3,5 +3,9 @@
 // select a document from the document collection ids
 
 // const worktime=document.getElementById("works-time")
-const link=document.querySelector("a")
-console.log(link.getAttribute("href").slice(1))
+// const link=document.querySelector("a")
+// console.log(link.getAttribute("href").slice(1))
+// link.style.backgroundColor="red"
+
+const user=document.getElementById("username")
+
