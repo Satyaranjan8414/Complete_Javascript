534c0c2d87997cdc0840e230b7fe290f9e58cc35
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-04 20:27:59 +0530

    DOM

--- class-2/Document Object Model/index1.js
+++ class-2/Document Object Model/index1.js
@@ -3,11 +3,13 @@ const rootnode=document.getRootNode();
 const htmlnode= rootnode.childNodes[0]
 //console.log(htmlnode.childNodes)//NodeList(3) [head, text, body]
 
-// const headnode=htmlnode.childNodes[0]
-// console.log(headnode)
+const headnode=htmlnode.childNodes[0]
+console.log(headnode.cloneNode)
 
 // const testnode=htmlnode.childNodes[1]
 // console.log(testnode)
 
-const bodynode=htmlnode.childNodes[2]
-console.log(bodynode)
+// const bodynode=htmlnode.childNodes[2]
+// console.log(bodynode)
+
+// 5:7--Traversing the tree
