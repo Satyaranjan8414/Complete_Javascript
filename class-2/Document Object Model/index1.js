8d20391b2c3c8474a3ba696d9893fea26cff9211
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-05 01:44:55 +0530

    added parentNode

--- class-2/Document Object Model/index1.js
+++ class-2/Document Object Model/index1.js
@@ -1,10 +1,10 @@
-const rootnode=document.getRootNode();
-// console.log(rootnode);
-const htmlnode= rootnode.childNodes[0]
+// const rootnode=document.getRootNode();
+// // console.log(rootnode);
+// const htmlnode= rootnode.childNodes[0]
 //console.log(htmlnode.childNodes)//NodeList(3) [head, text, body]
 
-const headnode=htmlnode.childNodes[0]
-console.log(headnode.cloneNode)
+// const headnode=htmlnode.childNodes[0]
+// console.log(headnode.cloneNode)
 
 // const testnode=htmlnode.childNodes[1]
 // console.log(testnode)
@@ -12,7 +12,10 @@ console.log(headnode.cloneNode)
 // const bodynode=htmlnode.childNodes[2]
 // console.log(bodynode)
 
-const h1=document.querySelector("h1")
-const h1parent= h1.parentNode
+// const h1=document.querySelector("h1")
+// const h1parent= h1.parentNode
 
-h1.style.backgroundColor= "green"
+// h1.style.backgroundColor= "green"
+
+const con=document.querySelector(".container")
+console.log(con.children)
