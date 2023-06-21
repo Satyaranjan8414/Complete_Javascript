4a3328ecb79944960149ce64fa358b4dc659fa34
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-21 17:17:27 +0530

    added Method

--- Method/index.js
+++ Method/index.js
@@ -2,15 +2,15 @@
 
 //Function inside Object
 
-// const person={
-//     firstname:"Ram",
-//     age:23,
-//     gender:"male",
-//     about: function(){
-//         console.log(`name is ${this.firstname} and age is ${this.age} and gender is ${this.gender}`)
-//     }
-// }
-// person.about()
+const person={
+    firstname:"Ram",
+    age:23,
+    gender:"male",
+    about: function(){
+        console.log(`name is ${this.firstname} and age is ${this.age} and gender is ${this.gender}`)
+    }
+}
+person.about()
 
 
 //Call APPLY Bind
