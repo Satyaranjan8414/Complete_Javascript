62003c021d71804ec94011a870951ae6cde2211b
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-23 01:12:07 +0530

    added new

--- Class-3/CallBacks/index.js
+++ Class-3/CallBacks/index.js
@@ -1,7 +1,59 @@
-function fun(callback){
-    console.log("Hi bro")
-    callback()
+// function fun(callback){
+//     console.log("Hi bro")
+//     callback()
+// }
+// fun(()=>{
+//     console.log("hello") 
+// })
+
+// function getNumber(a,b,callback){
+//     console.log(a,b)
+//     callback(a,b)
+// }
+
+// // function add(c,d){
+// //     console.log(c+d)
+// // }
+
+// getNumber(1,2,(c,d)=>{
+//     console.log(c+d)
+// })
+
+
+// function getNumber(num1,num2,onSuccess,onFail){
+//     if(typeof num1 === "number" && typeof num2 === "number"){
+//         onSuccess(num1,num2)
+//     }else {
+//         onFail()
+//     }
+
+// }
+
+// function addTwoNumbers(num1,num2){
+//     console.log(num1+num2)
+// }
+// function onFailure(){
+//     console.log("Please pass a number")
+// }
+
+// getNumber(100,2,addTwoNumbers,onFailure)
+
+
+function getNumberAndAddTwoNumbers(num1,num2,onSuccess,onFailure){
+    if(typeof num1 ==="number" && typeof num2 ==="number"){
+        onSuccess(num1,num2)
+    }else{
+        onFailure()
+    }
 }
-fun(()=>{
-    console.log("hello") 
-})
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2)
+}
+
+function onFailure(){
+    console.log("Please pass a number")
+    console.log("Type a Number value")
+}
+
+getNumberAndAddTwoNumbers(100,2,addTwoNumbers,onFailure)
