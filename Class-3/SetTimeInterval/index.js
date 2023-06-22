f034e7863a14c134af0cd72a62824424c256b765
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-22 18:05:00 +0530

    added SetTimeInterval

--- Class-3/SetTimeInterval/index.js
+++ Class-3/SetTimeInterval/index.js
@@ -1,9 +1,16 @@
 const body=document.body;
+const button=document.querySelector('button');
 
-setInterval(()=>{
+const IntervalID=setInterval(()=>{
 const red=Math.floor(Math.random()*100);
 const green=Math.floor(Math.random()*100);
 const blue=Math.floor(Math.random()*100);
 const rgb=`rgb(${red},${green},${blue})`;
 body.style.background=rgb;
 },1000)
+
+
+button.addEventListener('click',()=>{
+    clearInterval(IntervalID)
+    button.textContent=body.style.background;
+})
