41cabe87aa345f5e46c0e62dd91b0c56d8efc9ee
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-26 19:55:06 +0530

    addec PromiseInDeep

--- Class-3/CallBacks/Promise/index1.js
+++ Class-3/CallBacks/Promise/index1.js
@@ -1,18 +1,54 @@
 //Returning promise from function
 
-function promiseTo(){
-    const busket=['chips','ghi','rice','salt']
-    return new Promise((resolve, reject)=>{
-        if(busket.includes('salt')&& busket.includes('rice')&& busket.includes('ghi')){
-            resolve("FriedRice")
-        }else{
-            reject("NotFriedRice")
-        }
+// function promiseTo(){
+//     const busket=['chips','ghi','rice','salt']
+//     return new Promise((resolve, reject)=>{
+//         if(busket.includes('salt')&& busket.includes('rice')&& busket.includes('ghi')){
+//             resolve("FriedRice")
+//         }else{
+//             reject("NotFriedRice")
+//         }
+//     })
+// }
+
+// promiseTo().then((data)=>{
+//     console.log("Lets take the food")
+// }).catch((err)=>{
+//     console.log(err.message)
+// });
+
+
+//Promise with setTimeout
+
+// function myPromise(){
+//     return new Promise((resolve, reject)=>{
+//         const value=true;
+//             setTimeout(()=>{
+//                 if(value){
+//                     resolve()
+//                 }else{
+//                     reject()
+//                 }
+//             },2000)
+//         })
+// }
+
+// myPromise().then(()=>{console.log("Resolve")}).catch(()=>{console.log("Reject")});
+
+
+
+function mypromiseToYou(){
+    return new Promise ((resolve, reject)=>{
+        const value=false;
+        setTimeout(()=>{
+            if(value){
+                resolve()
+                console.log("i am waiting for you")
+            }else{
+                reject()
+            }
+        },100)
     })
 }
 
-promiseTo().then((data)=>{
-    console.log("Lets take the food")
-}).catch((err)=>{
-    console.log(err.message)
-});
+mypromiseToYou().then(()=>{console.log("let's Go !")}).catch(()=>{console.log("You wasted my time")});
