1f784d5d0235bbb87e8ee91fdc904c884ced80f9
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-29 16:07:34 +0530

    added TemplatelITERAL

--- Es6-Full-Concept-WithInterviews/index.js
+++ Es6-Full-Concept-WithInterviews/index.js
@@ -1,7 +1,32 @@
 // console.log("Es6 Series")
 //Use of var and let instead
-for(var i=0;i<10;i++){
-    setTimeout(()=>{
-        console.log(i)
-    },1000)
-}
+
+
+// for(var i=0;i<10;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1000)
+// }
+
+//Default Parameters
+// function Default(){
+//     return 100;
+// }
+
+// function add(a,b=Default()){
+//     return a+b;
+// }
+// console.log(add(10))
+
+// function main(a,b=main()){
+//     return a+b;
+// }
+// console.log(main());//Maximum call stack size exceeded
+
+//tempklate Laterals
+
+let quote="Either You Run the code or code will Run You"
+let by="Satyaranjan -M"
+
+let code=`${quote} ${by} `
+console.log(code)
