4ad59950bd97533025ba894aa671b070c0cd1fcb
Satyaranjan8414 <satya2maity@gmail.com>
2023-06-29 18:30:12 +0530

    adeed Es6Concept

--- Es6-Full-Concept-WithInterviews/index.js
+++ Es6-Full-Concept-WithInterviews/index.js
@@ -25,8 +25,66 @@
 
 //tempklate Laterals
 
-let quote="Either You Run the code or code will Run You"
-let by="Satyaranjan -M"
+// let quote="Either You Run the code or code will Run You"
+// let by="Satyaranjan -M"
 
-let code=`${quote} ${by} `
-console.log(code)
+// let code=`${quote} ${by} `
+// console.log(code)
+
+
+//Find and findindex function
+// let arr=[
+//     {id:10},
+//     {id:20},
+//     {id:46},
+//     {id:100},
+// ]
+
+// let res=arr.findIndex((item)=>{
+//     return item.id
+// })
+// console.log(res)
+
+// class Fruits{
+//     name="Orange"
+//     constructor(){
+//         console.log(this.name)
+//     }
+// }
+// let getFruits= new Fruits()
+// // console.log(getFruits)
+
+
+// Inheritance
+
+class color{
+    black(){
+        return "The color is black"
+    }
+    legs(){
+        console.log("4 legs are there")
+    }
+}
+
+class animal{
+    
+    constructor(){
+       
+            console.log("constructor")
+        }
+    eat(){
+        console.log("eat")
+    }
+    sleep(){
+        console.log("sleep")
+    }
+}
+
+let animalcall= new animal()
+let call= new color()
+
+// animalcall.legs()
+animalcall.eat()
+animalcall.sleep()
+call.legs()
+
